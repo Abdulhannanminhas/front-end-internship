708e09ed1757c0bc2a14abd0b9bde9936a0ea4bc
Abdulhannanminhas <130302390+Abdulhannanminhas@users.noreply.github.com>
2025-11-11 20:58:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile Menu Toggle
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav ul');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
